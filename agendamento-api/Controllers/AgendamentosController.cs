24d4c97d6d28a1dcc16de1852bd646b3e42dbb15
gabrielwatanabe043 <gabrielpereira569@hotmail.com>
2023-10-25 22:18:33 -0300

    Concluído controller agendamento e validação se data existe

--- agendamento-api/Controllers/AgendamentosController.cs
+++ agendamento-api/Controllers/AgendamentosController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using agendamento_api.Data;
 using agendamento_api.Models;
 using agendamento_api.DtosRequest;
+using agendamento_api.DtoResponse;
 
 namespace agendamento_api.Controllers
 {
@@ -24,45 +25,91 @@ namespace agendamento_api.Controllers
 
         // GET: api/Agendamentos
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Agendamento>>> GetAgendamentos()
+        public async Task<ActionResult<IEnumerable<AgendamentoResponse>>> GetAgendamentos()
         {
           if (_context.Agendamentos == null)
           {
               return NotFound();
           }
-            return await _context.Agendamentos.ToListAsync();
+            var agendamentos = await _context.Agendamentos.ToListAsync();
+            List<AgendamentoResponse> agendamentoResponses = new List<AgendamentoResponse>();
+            foreach (var item in agendamentos) 
+            {
+                var servico = await _context.Servicos.FindAsync(item.ServicoId);
+
+                AgendamentoResponse agendamento = new AgendamentoResponse();
+                agendamento.Id = item.Id;
+                agendamento.Data = item.Data;
+                agendamento.StatusId = item.StatusId;
+                agendamento.StatusNome = _context.Status.Where(x => x.Id == item.StatusId).Select(x => x.Nome).FirstOrDefault();
+                agendamento.ClienteId = item.ClienteId;
+                agendamento.ClienteNome = _context.Clientes.Where(x => x.Id == item.ClienteId).Select(x => x.Nome).FirstOrDefault();
+                agendamento.ServicoId = item.ServicoId;
+                agendamento.ServicoNome = _context.Servicos.Where(x => x.Id == item.ServicoId).Select(x => x.Nome).FirstOrDefault();
+                agendamento.ProfissionalId = servico.ProfissionalId;
+                agendamento.ProfissionalNome = _context.Profissionais.Where(x => x.Id == servico.ProfissionalId).Select(x => x.Nome).FirstOrDefault();
+                agendamento.Valor = item.Servicos.Valor;
+                agendamentoResponses.Add(agendamento);
+            }
+            return agendamentoResponses.OrderBy(x => x.Id).ToList();
         }
 
         // GET: api/Agendamentos/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Agendamento>> GetAgendamento(int id)
+        public async Task<ActionResult<AgendamentoResponse>> GetAgendamento(int id)
         {
           if (_context.Agendamentos == null)
           {
               return NotFound();
           }
             var agendamento = await _context.Agendamentos.FindAsync(id);
-
             if (agendamento == null)
             {
                 return NotFound();
             }
 
-            return agendamento;
+            var servico = await _context.Servicos.FindAsync(agendamento.ServicoId);
+
+            AgendamentoResponse agendamentoResponse = new AgendamentoResponse();
+            agendamentoResponse.Id = agendamento.Id;
+            agendamentoResponse.Data = agendamento.Data;
+            agendamentoResponse.StatusId = agendamento.StatusId;
+            agendamentoResponse.StatusNome = _context.Status.Where(x => x.Id == agendamento.StatusId).Select(x => x.Nome).FirstOrDefault();
+            agendamentoResponse.ClienteId = agendamento.ClienteId;
+            agendamentoResponse.ClienteNome = _context.Clientes.Where(x => x.Id == agendamento.ClienteId).Select(x => x.Nome).FirstOrDefault();
+            agendamentoResponse.ServicoId = agendamento.ServicoId;
+            agendamentoResponse.ServicoNome = _context.Servicos.Where(x => x.Id == agendamento.ServicoId).Select(x => x.Nome).FirstOrDefault();
+            agendamentoResponse.ProfissionalId = servico.ProfissionalId;
+            agendamentoResponse.ProfissionalNome = _context.Profissionais.Where(x => x.Id == servico.ProfissionalId).Select(x => x.Nome).FirstOrDefault();
+            agendamentoResponse.Valor = agendamento.Servicos.Valor;
+
+            return agendamentoResponse;
         }
 
         // PUT: api/Agendamentos/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutAgendamento(int id, Agendamento agendamento)
+        public async Task<IActionResult> PutAgendamento(int id, AgendamentoDto agendamentoDto)
         {
-            if (id != agendamento.Id)
+            var agendamento = await _context.Agendamentos.FindAsync(id);
+            if (agendamento == null) 
             {
-                return BadRequest();
+                return NotFound();
+            }
+            
+
+            if (DataExists(agendamentoDto.ServicoId, agendamentoDto.Data.Trim()) && agendamentoDto.Data.Trim() != agendamento.Data.Trim())
+            {
+                return BadRequest("Já existe um agendamento neste horário");
             }
 
-            _context.Entry(agendamento).State = EntityState.Modified;
+            agendamento.Data = agendamentoDto.Data;
+            agendamento.ServicoId = agendamentoDto.ServicoId;
+            agendamento.ClienteId = agendamentoDto.ClienteId;
+            agendamento.StatusId = agendamentoDto.StatusId;
 
+            _context.Entry(agendamento).State = EntityState.Modified;
+                
             try
             {
                 await _context.SaveChangesAsync();
@@ -91,6 +138,11 @@ namespace agendamento_api.Controllers
           {
               return Problem("Entity set 'AgendamentoContext.Agendamentos'  is null.");
           }
+
+            if (DataExists(agendamentoDto.ServicoId, agendamentoDto.Data)) 
+            {
+                return BadRequest("Já existe um agendamento neste horário");
+            }
             Agendamento agendamento = new Agendamento(agendamentoDto.Data, agendamentoDto.ServicoId, agendamentoDto.ClienteId, agendamentoDto.StatusId);
             _context.Agendamentos.Add(agendamento);
             await _context.SaveChangesAsync();
@@ -123,6 +175,21 @@ namespace agendamento_api.Controllers
         private bool AgendamentoExists(int id)
         {
             return (_context.Agendamentos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        private  bool DataExists(int idServico, string data) 
+        {
+            var dataExists = _context.Agendamentos.ToList();
+             
+            foreach (var item in dataExists) 
+            {
+                if (item.ServicoId == idServico && item.Data.Trim() == data.Trim())
+                {
+                    return true ;
+                    
+                }
+            }
+            return false;
         }
     }
 }
--- agendamento-api/DtoResponse/AgendamentoResponse.cs
+++ agendamento-api/DtoResponse/AgendamentoResponse.cs
@@ -1,6 +1,38 @@
-﻿namespace agendamento_api.DtoResponse
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+using System.Security.Cryptography.X509Certificates;
+
+namespace agendamento_api.DtoResponse
 {
     public class AgendamentoResponse
     {
+        public int Id { get; set; }
+        public string Data { get; set; }
+        public int StatusId {get;set;}
+        public string StatusNome { get; set; }
+        public int ClienteId { get; set; }
+        public string ClienteNome { get; set;}
+        public int ServicoId { get; set; }
+        public string ServicoNome { get; set;}
+        public int ProfissionalId { get; set; }
+        public string ProfissionalNome { get; set; }
+        public double Valor { get; set; }
+
+        public AgendamentoResponse() 
+        {
+        }
+        public AgendamentoResponse(int id, string data, int statusId, string statusNome, int clienteId, string clienteNome, int servicoId, string servicoNome, int profissionalId, string profissionalNome, double valor)
+        {
+            Id = id;
+            Data = data;
+            StatusId = statusId;
+            StatusNome = statusNome;
+            ClienteId = clienteId;
+            ClienteNome = clienteNome;
+            ServicoId = servicoId;
+            ServicoNome = servicoNome;
+            ProfissionalId = profissionalId;
+            ProfissionalNome = profissionalNome;
+            Valor = valor;
+        }
     }
 }
